b4bd675ff0a9d429d83f98738124f354c991969d
keqh <keqh.keqh@gmail.com>
2012-10-30 21:27:14 +0900

    とりあえずかっこへらした

--- 2012-10-30/kei_q/148.hs
+++ 2012-10-30/kei_q/148.hs
@@ -3,21 +3,29 @@ import Data.Monoid ((<>))
 import Text.Printf (printf)
 
 data Token = N Double | BinOp String deriving (Eq)
-data Expr = Leaf Token | Node Expr Token Expr deriving (Eq)
+data Expr = Leaf Double | Node Expr Token Expr deriving (Show, Eq)
 
 instance Show Token where
     show (N n) = show n
     show (BinOp op) = op
 
-instance Show Expr where
-    show (Leaf n) = show n
-    show (Node el t er) = printf "(%s %s %s)" (show el) (show t) (show er)
+toString :: Token -> Expr -> String
+toString _ (Leaf n) = show n
+toString p (Node el t er) 
+  | check p t = printf "(%s %s %s)" (toString t el) (show t) (toString t er)
+  | otherwise = printf "%s %s %s" (toString t el) (show t) (toString t er)
+
+check (BinOp "*") (BinOp "+") = True
+check (BinOp "*") (BinOp "-") = True
+check (BinOp "/") (BinOp "+") = True
+check (BinOp "/") (BinOp "-") = True
+check _ _ = False
 
 main :: IO ()
 main = getLine >>= putStrLn . solve
 
 solve :: String -> String
-solve = show . parse . tokenizer
+solve = toString (BinOp "dummy") . parse . tokenizer
 
 tokenizer :: String -> [Token]
 tokenizer = map toToken . words
@@ -29,5 +37,8 @@ toToken t = case readFloat t of
 
 parse :: [Token] -> Expr
 parse = head . foldl aux [] where
-    aux expr t@(N n) = Leaf t : expr
+    aux expr (N n) = Leaf n : expr
     aux (a:b:expr) t = Node b t a : expr
+
+-- (1 * 2 + 3) * 4  #  1 2 * 3 + 4 *
+-- (1 + 2 * 3) * 4  #  1 2 3 * + 4 *
