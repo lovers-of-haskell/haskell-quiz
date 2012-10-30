dc3acb1ce84637fa3b33eead3af4a1e2e80bbe1f
willnet <netwillnet@gmail.com>
2012-10-30 21:12:36 +0900

    Add ruby quiz 148 by willnet

---
+++ 2012-10-30/willnet/quiz148.hs
@@ -0,0 +1,25 @@
+data Item = Num String | Division | Times | Minus | Plus deriving Show
+
+strToItem :: String -> Item
+strToItem "/" = Division
+strToItem "*" = Times
+strToItem "-" = Minus
+strToItem "+" = Plus
+strToItem x = Num x
+
+strToItemList :: String -> [Item]
+strToItemList = reverse . foldl (\acc x -> strToItem x : acc) [] . words
+
+make = head . foldl culc [] . strToItemList
+  where
+    culc (x:y:xs) Division = (y ++ " / " ++ x) : xs
+    culc (x:y:xs) Times = (y ++ " * " ++ x) : xs
+    culc (x:y:xs) Minus = ("(" ++ y ++ " - " ++ x ++ ")") : xs
+    culc (x:y:xs) Plus = ("(" ++ y ++ " + " ++ x ++ ")") : xs
+    culc xs (Num x)= x : xs
+
+
+
+
+
+
